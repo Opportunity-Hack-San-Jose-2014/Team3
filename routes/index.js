fe4b7589bb0c80491310ce3575f48d487e79445c
Ruiyun <farcryzry@gmail.com>
2014-10-12 00:05:46 -0700

    dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,26 @@ var router = express.Router();
 var mongoose = require("mongoose");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  //mongoose.connect('mongodb://localhost/spartanfly');
-  console.log(JSON.stringify(req.session.user));
-  if(req.session.user == undefined){
-  	req.session.user = {username: "fengjiawei"};
-  	res.render('index', { title: '11', user: req.session.user});
-  }else{
-  	res.send("hi i am "+JSON.stringify(req.session.user));
-  }
+router.get('/', function (req, res) {
+    //mongoose.connect('mongodb://localhost/spartanfly');
+    console.log(JSON.stringify(req.session.user));
+    if (req.session.user == undefined) {
+        req.session.user = {username: "fengjiawei"};
+        res.render('index', { title: '11', user: req.session.user});
+    } else {
+        //res.send("hi i am "+JSON.stringify(req.session.user));
+        res.render('index', { title: '11', user: req.session.user});
+    }
 });
 
-router.get('/plan', function(req, res) {
-  res.render('business-plan');
+router.get('/plan', function (req, res) {
+    res.render('business-plan');
 });
+
+router.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+});
+
 
 
 module.exports = router;
